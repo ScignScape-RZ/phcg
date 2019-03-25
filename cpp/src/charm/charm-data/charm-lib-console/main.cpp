53349aed4d7dd8c8e733660bf26d5d5d08864be8
ScignScape-RZ <osrworkshops@gmail.com>
2019-03-24 20:46:40 -0400

    update

---
+++ cpp/src/charm/charm-data/charm-lib-console/main.cpp
@@ -0,0 +1,19 @@
+
+//          Copyright Nathaniel Christen 2019.
+// Distributed under the Boost Software License, Version 1.0.
+//    (See accompanying file LICENSE_1_0.txt or copy at
+//          http://www.boost.org/LICENSE_1_0.txt)
+
+
+//#include "kans.h"
+
+//USING_KANS(KDMI)
+
+#include <QDebug>
+
+
+int main(int argc, char **argv)
+{
+ qDebug() << "ok";
+ return 0;
+}
