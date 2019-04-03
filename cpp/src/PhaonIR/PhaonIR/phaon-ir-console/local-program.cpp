c85330fc959dead7904cb58a5792e1e2b524bc2b
ScignScape-RZ <osrworkshops@gmail.com>
2019-04-03 15:20:13 -0400

    update

--- cpp/src/PhaonIR/PhaonIR/phaon-ir-console/local-program.cpp
+++ cpp/src/PhaonIR/PhaonIR/phaon-ir-console/local-program.cpp
@@ -20,7 +20,7 @@
 
 #include <QDebug>
 
-
+#ifdef HIDE
 void local_program(PhaonIR& phr)
 {
  phr.init_type_system();
@@ -74,13 +74,39 @@ void local_program(PhaonIR& phr)
  phr.reset_program_stack();
 
 }
+#endif // HIDE
 
-#ifdef HIDE
-void local_program1(PhaonIR& phr)
+//#ifdef HIDE
+void local_program(PhaonIR& phr)
 {
  phr.init_type_system();
- phr.init_type("fbase");
- phr.init_type("u4");
+ phr.init_type("fbase", DEFAULT_PTR_BYTE_CODE);
+ phr.init_type("u4", 4);
+
+
+ // //  setup
+ phr.init_code_model();
+
+ PHR_Code_Model& pcm = *phr.code_model();
+ pcm.set_type_system(phr.type_system());
+
+ phr.create_channel_semantic_protocol("lambda");
+ phr.create_channel_semantic_protocol("result");
+
+ pcm.set_direct_eval_fn(&phr_direct_eval);
+
+ PHR_Runtime_Scope prs(nullptr);
+
+ PHR_Symbol_Scope pss(&prs);
+
+ phr.init_table();
+
+// PHR_Runner phrn;
+// phrn.
+
+ init_test_functions(phr, pcm, *phr.table(), pss);
+ //phrn.get_runtime_scope_queue().push_front(&prs);
+ // //  setup
 
  phr.init_program_stack();
 
@@ -119,7 +145,7 @@ void local_program1(PhaonIR& phr)
  phr.coalesce_channel_group();
  phr.copy_anchor_channel_group("x", "result");
 
- phr.evaluate_channel_group();
+ phr.evaluate_channel_group(&pss);
 
  phr.delete_temps();
  phr.delete_retired();
@@ -138,10 +164,25 @@ void local_program1(PhaonIR& phr)
  phr.push_carrier_symbol("x");
  phr.coalesce_channel_group();
 
- phr.evaluate_channel_group();
+ phr.evaluate_channel_group(&pss);
+
  phr.delete_temps();
  phr.delete_retired();
- phr.delete_temps();
  phr.clear_temps();
+
+ phr.reset_program_stack();
+
+ phr.push_carrier_stack("fuxe");
+ phr.hold_type_by_name("fbase");
+ //?phr.push_carrier_raw_value("&prn");
+ phr.push_carrier_symbol("&prn");
+
+ phr.push_carrier_stack("lambda");
+ phr.hold_type_by_name("u4");
+ phr.push_carrier_symbol("x");
+
+ phr.coalesce_channel_group();
+ phr.evaluate_channel_group(&pss);
+
 }
-#endif
+//#endif
